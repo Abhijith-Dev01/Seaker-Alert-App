4397d9d65eb56c9246597f036af611783ff66f25
Abhijith Sony <abhis3337@gmail.com>
2025-09-22 13:23:13 +0530

    alembic deleted

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-Database_url = 'postgresql+psycopg2://postgres:postgres@db:5432/system_monitoring'
-# Database_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/system_monitoring'
+# Database_url = 'postgresql+psycopg2://postgres:postgres@db:5432/system_monitoring'
+Database_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/system_monitoring'
 
 engine = create_engine(Database_url)
 sessionLocal = sessionmaker(autoflush=True,autocommit=False,bind=engine)
